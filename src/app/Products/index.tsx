1435732f529d4628c5aba3ffec095c7a4513d433
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-11-09 09:02:18 -0300

    Added ingredients and Buy button.

--- src/app/Products/index.tsx
+++ src/app/Products/index.tsx
@@ -7,38 +7,44 @@ import { useBackground } from "@/lib/providers/BackgroundProvider";
 import { useSpring } from "@react-spring/three";
 import { useCan } from "@/lib/providers/CanProvider";
 import { useMotionValue, useMotionValueEvent, useTransform } from "framer-motion";
+import Button from "@/components/Button";
 
 const flavors = [
     {
         name: 'Fruit Punch',
         color: "#fc309d",
         background: 'linear-gradient(45deg, #8f060e, #C850C0)',
-        description: 'Delicious juice extracted by violently beating fruits up.'
+        description: 'Delicious juice extracted by violently beating fruits up.',
+        ingredients: 'Fruits, brass knuckles, sugar and isopropanol.'
     },
     {
         name: 'Lemon',
         color: '#00ff22',
         /* background: '#00ff00', */
         background: "linear-gradient(45deg, #00ab50, #00d19d)",
-        description: 'Intense lemon flavor in every sip!'
+        description: 'Intense lemon flavor in every sip!',
+        ingredients: '2 lemons, enriched uranium and a hint of ligma (synthetic).'
     },
     {
         name: 'Blueberry',
         color: '#6600ff',
         background: "linear-gradient(45deg, rgba(49,37,129,1), rgba(119,37,129,1))",  
-        description: 'Grapes but poggers!'
+        description: 'Grapes but poggers!',
+        ingredients: 'Cesium-137, methylene blue, and a berry we found on a bush.'
     },
     {
         name: 'Beans',
         color: '#964b00',
         background: "linear-gradient(45deg, rgba(67,45,39,1), rgba(140,104,59,1))",
-        description: 'Beans, the new sensation in your mouth! 🤤'
+        description: 'Beans, the new sensation in your mouth! 🤤',
+        ingredients: 'Beans'
     },
     {
         name: 'Pineapple',
         color: '#e3c230',
         background: "linear-gradient(45deg, #e3c230, rgba(133,232,111,1))",
-        description: "A mix of sweet with a sour touch.\nA real happiness punch.\n(Unless you're allergic.)"
+        description: "A mix of sweet with a sour touch.\nA real happiness punch.\n(Unless you're allergic.)",
+        ingredients: 'Spongebob & Friends, 7 moles of sulphuric acid.'
     }
 ]
 
@@ -65,10 +71,6 @@ const Component = () => {
         return resetBackground;
     }, []);
 
-    useMotionValueEvent(swipeProgress, "change", (v) => {
-        console.log(v);
-    })
-
     useMotionValueEvent(canColor, "change", (v) => {
         setColor(canColor);
     });
@@ -90,14 +92,19 @@ const Component = () => {
                     flavors.map((flavor, i) => (
                         <SwiperSlide key={i} className="w-100 h-100">
                             <div
-                                className="position-absolute d-flex flex-column justify-content-center align-items-left"
+                                className="position-absolute d-flex gap-0 flex-row justify-content-between align-items-center w-full"
                                 style={{
                                     left: '5rem',
+                                    right: '5rem',
                                     bottom: '5rem'
                                 }}
                             >
-                                <h1 style={{ color: '#fff' }}>{flavor.name}</h1>
-                                <p style={{ color: '#fff', maxWidth: '40rem' }} className="fs-4">{flavor.description}</p>
+                                <div className="d-flex gap-0 flex-column align-items-left justify-content-center">
+                                    <h1 style={{ color: '#fff', lineHeight: 1 }}>{flavor.name}</h1>
+                                    <p style={{ color: '#fff', maxWidth: '40rem' }} className="fs-4">{flavor.description}</p>
+                                    <p style={{ color: '#fff', maxWidth: '40rem', margin: 0 }} className="fs-6">Ingredients: {flavor.ingredients}</p>
+                                </div>
+                                <Button style={{ background: flavor.color, fontSize: '3rem' }}>Buy</Button>
                             </div>
                         </SwiperSlide>
                     ))
