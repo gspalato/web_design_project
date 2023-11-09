e113e4001ac66c6eec1d6e54b98fb1982f087ca6
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-11-09 08:19:22 -0300

    Background color transitions.

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -8,26 +8,30 @@ import SodaCanScene from "@components/SodaCanScene";
 import { useCan } from "@/lib/providers/CanProvider";
 
 import App from "./App";
+import { useBackground } from "@/lib/providers/BackgroundProvider";
 
 const Layout: React.FC<React.PropsWithChildren> = () => {
     const { color, visible } = useCan();
+    const { background } = useBackground();
 
     return (
         <BrowserRouter>
-            <motion.div
-                className="position-absolute h-100 w-100"
-                animate={{ opacity: visible ? 1 : 0 }}
-                transition={{ duration: 0.5 }}
-            >
-                <SodaCanScene className="h-100 w-100" color={color} />
+            <motion.div className="h-100 w-100" animate={{ background }}>
+                <motion.div
+                    className="position-absolute h-100 w-100"
+                    animate={{ opacity: visible ? 1 : 0 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    <SodaCanScene className="h-100 w-100" color={color} />
+                </motion.div>
+                <div className="pb-0 vh-100 w-100 position-absolute overflow-y-scroll">
+                    <Header />
+                    <main className="d-flex justify-content-center align-items-center flex-column h-100 w-100">
+                        <App />
+                    </main>
+                    <Footer />
+                </div>
             </motion.div>
-            <div className="pb-0 vh-100 w-100 position-absolute overflow-y-scroll">
-                <Header />
-                <main className="d-flex justify-content-center align-items-center flex-column h-100 w-100">
-                    <App />
-                </main>
-                <Footer />
-            </div>
         </BrowserRouter>
     )
 }
